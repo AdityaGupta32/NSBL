1a0fb4b4c9238ea1f568a97164c5029f5a18987c
Aditya Lallan Gupta <97335636+AdityaGupta32@users.noreply.github.com>
2025-12-06 21:55:05 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const MONGO_URI = "mongodb+srv://aditya22:aditya123@nsbl2.f5wxidl.mongodb.net/nsbl2?appName=nsbl2&retryWrites=true&w=majority";
+
 connectDB = async () => {
     try {
       // We connect directly using the hardcoded string
-      const conn = await mongoose.connect(MONGO_URI);
+      const conn = await mongoose.connect(process.env.MONGO_URI);
       console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
       console.error(`❌ Database Connection Error: ${error.message}`);
@@ -11,5 +11,4 @@ connectDB = async () => {
     }
   };
   
-
   module.exports = connectDB;
